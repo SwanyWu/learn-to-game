0ed192388fc0625ec8d94a317f7cc153bd29feac
SwanyWu <wuhuiyao96@163.com>
2023-03-02 19:49:15 +0100

    comments and diagram modified

--- classes/Level.py
+++ classes/Level.py
@@ -11,6 +11,7 @@ from entities.Koopa import Koopa
 from entities.CoinBox import CoinBox
 from entities.RandomBox import RandomBox
 
+# define relative position/level to other entities
 
 class Level:
     def __init__(self, screen, sound, dashboard):
